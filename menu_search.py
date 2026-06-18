f03c0862cabc16f751259ece14789739979002f3
Rohit Rao <rohitrao@Rohits-MacBook-Air.local>
2026-06-18 10:43:12 +0530

    Third Time's the charm

--- menu_search.py
+++ menu_search.py
@@ -4,50 +4,61 @@
 #For version 1 we will assume that the food item ordered is available in the shop 
 
 from dotenv import load_dotenv
-from pypdf import PdfReader
 from tabulate import tabulate
 import re
 import os, sys
 import json
 _ = load_dotenv()
 from rich import print_json
+import base64
+from pydantic import BaseModel, Field
+import fitz
 
 from openai import OpenAI
 Client = OpenAI()
 
 structured_menu = [] #it is a list of dictionaries of the form {"food item":";"price"}
 order_list = [] #It is also to be a list of dictionaries having 3 values - food_item, price, quantity
+table_rows = []  #this is list of all the food item name and their corresponding prices
+price_lookup = {} #A dictionary to store the corresponding the food item - price mapping
+
+def ready_menu(menu_list):
+    for food in menu_list["items"]:
+        table_rows.append([
+            food["name"],
+            food["price"],
+        ])
+    
+    for food in table_rows:
+        price_lookup[food[0].lower()] = food[1]
+    
 
 
-def show_menu():
-    reader = PdfReader("Menu.pdf")
+def print_menu():
+    print("Printing from Show Menu")
+    headers = ["Item Name","Item Price"]
+    print(tabulate(table_rows, headers=headers, tablefmt="grid", colalign=("left", "centre")))
+    return ("Here's your menu. Please let me know what would you like to have today?")
+   #print(table_rows)
 
-    for page in reader.pages:
-        text = page.extract_text() or ""
-        #print(page)
 
-        for line in text.splitlines():
-            print(line)
-            match = re.fullmatch(
-                r"(.+?)\s*\.{2,}\s*\$?(\d+(?:\.\d{2})?)",
-                line.strip()
-            )
+def check_item(food_item):
+    print(f"checking item {food_item}")
+    if(food_item in price_lookup):
+        return 1
+    else:
+        return 0
 
-            if match:
-                key = match.group(1).strip().lower()
-                price_text = match.group(2)
-                price = float(price_text) if "." in price_text else int(price_text)
-                structured_menu.append({key: price})
+def get_price(food_item):
+    print(f"getting the price of {food_item}")
+    if(check_item(food_item)):
+        return(price_lookup[food_item])
+    else:
+        return("The ordered food item does not exist")
 
-def check_price(food_item):
-    for item in structured_menu:
-        for food in item:
-            if (food == food_item):
-                return int(item[food])
-    return 0
 
 def print_final_bill():
-    print("\033[H\033[J", end="")
+    #print("\033[H\033[J", end="")
     print("Here is your total bill from the function! ")
     bill_total = 0
     table_rows = []
@@ -64,6 +75,7 @@ def print_final_bill():
     print(tabulate(table_rows, headers=headers, tablefmt="grid"))
     print(f"Your total order is {bill_total}")
     print(" \n \n")
+    sys.exit()
 
 def done_ordering():
     print("Thank you for ordering with us today. bon apetitie! ")
@@ -72,12 +84,12 @@ def done_ordering():
 
 def cancel_everything():
     order_list.clear()
-    print("I have successfully cleared all your order list")
-    return
+    return("I have successfully cleared all your order list")
 
 
 def cancel_order(food_item):
     bool = False
+    food_item = food_item.strip().lower()
     for food in order_list:
         if(food["food_item"]==food_item):
             bool = True
@@ -86,11 +98,9 @@ def cancel_order(food_item):
             order for order in order_list
             if order["food_item"] != food_item
             ]
-        print(f"The order for {food_item} has been successfully cancelled" )
-        return
+        return(f"The order for {food_item} has been successfully cancelled" )
     else:
-        print("Please place your order first before you can cancel the order")
-        return
+        return("Please place your order first before you can cancel the order")
 
 
 def add_order(food_item, quantity = 1):
@@ -99,38 +109,31 @@ def add_order(food_item, quantity = 1):
     for food in order_list:
         if (food["food_item"].strip().lower() == food_item):
             food["quantity"] += quantity
-            food["bill"] = food["quantity"] * check_price(food_item)
-            print("Your order has been updated")
-            return
-    print("Please place the order first before updating")
-    return
-
-def update_order(food_item, quantity = 1):
-    food_item = food_item.strip().lower()
-
-    for food in order_list:
-        if (food["food_item"].strip().lower() == food_item):
-            food["quantity"] = quantity
-            food["bill"] = food["quantity"] * check_price(food_item)
-            print("Your order has been updated")
-            return
-    print("Please place the order first before updating")
+            food["bill"] = food["quantity"] * get_price(food_item)
+            return("Your order has been updated")
+        else:
+            take_order(food_item, quantity)
     return
 
 
 def take_order(food_item, quantity = 1):
     "This function takes in the user order"
-    #first we check the price of that item
-    price = check_price(food_item)
+    print("Taking in the order now")
+    food_item = food_item.strip().lower()
+    
+    available = check_item(food_item)
+    print(f"the food item is {available}")
 
-    if price != 0:
+    if available:
         order_list.append({
             "food_item": food_item,
             "quantity" : quantity,
-            "bill": price*quantity
+            "bill": get_price(food_item)*quantity
         })
+        return("Your order has been successfully placed! ")
     else:
-        print(f"I do not what {food_item} is. Please check your spelling again. I am not placing that order currently")
+        return("The ordered food item does note exist")
+
 
 available_tools = [
     {
@@ -138,13 +141,7 @@ available_tools = [
         "function":
         {
             "name": "take_order",
-            "description": "The role of this function is to take the order of the user. What is essentially does is "
-            "that it takes in the name of the food item,  the quantitiy required and a list of dictionaries that"
-            "contains the orders till now. It then accesses the menu via the check_price function to see"
-            "whether we serve that particular food item or not. If we do serve, it multiplies the price of that food item"
-            "with the quantitiy required and returns an updated list. If we do not serve that food item, it simply"
-            "says that sorry we do not serve your required food item",
-
+            "description": "The role of this function is to take the order of the user.",
             "parameters":
             {
                 "type":"object",
@@ -170,32 +167,8 @@ available_tools = [
     {
         "type": "function",
         "function": {
-            "name":"update_order",
-            "description":"This function is used to update the order of the user.",
-            "parameters":
-            {
-                "type":"object",
-                "properties":
-                {
-                    "food_item":{
-                        "type":"string",
-                        "description":"This field contains the name of the food item that the user wants to"
-                        "update the order for"
-                    },
-                    "quantity":{
-                        "type":"integer",
-                        "description":"This is the updated quantity of the food item that the user wants to order"
-                },
-            },
-            "required":["food_item", "quantity"]
-        }
-    }
-    },
-    {
-        "type": "function",
-        "function": {
             "name":"add_order",
-            "description":"This function is used to add more items to the user's already existing order.",
+            "description":"This function is used to add more items to the user's order.",
             "parameters":
             {
                 "type":"object",
@@ -221,6 +194,48 @@ available_tools = [
             "name":"cancel_order",
             "description":"This function is used to cancel the order for a particular food item by the user. It takes "
             "as input the name of the food item if the food item exists and deletes it from the order list.",
+            "parameters":
+            {
+                "type":"object",
+                "properties":
+                {
+                    "food_item":{
+                        "type":"string",
+                        "description":"This field contains the name of the food item that the user wants to"
+                        "update the order for"
+                    },
+                
+            },
+            "required":["food_item"]
+        }
+    }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name":"get_price",
+            "description":"This functions returns the price of an ordered food item",
+            "parameters":
+            {
+                "type":"object",
+                "properties":
+                {
+                    "food_item":{
+                        "type":"string",
+                        "description":"This field contains the name of the food item that the user wants to"
+                        "update the order for"
+                    },
+                
+            },
+            "required":["food_item"]
+        }
+    }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name":"check_item",
+            "description":"This functions checks if a food item exists in the menu or not",
             "parameters":
             {
                 "type":"object",
@@ -273,23 +288,108 @@ available_tools = [
                 "properties": {},
             }
     }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name":"print_menu",
+            "description":"This function prints the menu.",
+            "parameters": {
+                "type": "object",
+                "properties": {},
+            }
+    }
     }
 ]
 
 
+class menu_data(BaseModel):
+    item_name: str = Field(description="The name of each food item in the menu provided")
+    price: float = Field(description="The price of each food item in the corresponding menu")
+
+def pdf_to_png(path):
+    doc = fitz.open(path)
+    images = []
+
+    for page in doc:
+        pix = page.get_pixmap(dpi = 200)
+        images.append(base64.b64encode(pix.tobytes("png")).decode())
+    
+    return images
+
+def extract_menu_from_images(images):
+    content = [
+        {
+            "type":"text",
+            "text":"You are smart text extractor agent. Your job is to extract the name of the food items from"
+            "the menu provided along with the price of each food item. Return ONLY valid JSON matching this schema: "
+            '{"items": [{"name": "...", "price": ...}]}'
+        }
+    ]
+
+    for img in images:
+        content.append(
+            {
+                "type":"image_url",
+                "image_url":{"url":f"data:image/png;base64,{img}"}
+            }
+        )
+    
+    response = Client.beta.chat.completions.parse(
+        model="gpt-4o",
+        messages=[
+            {
+                "role":"user",
+                "content": content
+            }
+        ],
+        response_format={"type":"json_object"}
+    )
+
+    raw = response.choices[0].message.content
+
+    return raw
+
+
 def main():
     "this is the main function"
-    show_menu()
-    print("Order bolo Order Order oooooorrrrrdddddeeeeerrrrrrrrrrrrrrrrrrrr")
+
+   #print("\033[H\033[J", end="")
+    print("/n /n /n")
+    print("New Code Running")
+
+    path = "Menu_img_1.jpg"
+
+    images = pdf_to_png(path)
+    menu_list = extract_menu_from_images(images)
+    menu_list = json.loads(menu_list)
+    #print(menu_list)
+    ready_menu(menu_list)
+
+    print(table_rows)
+    print("**************************")
+    print(price_lookup)
+
+    print("Please place your order")
+    user_input = input().strip().lower()
+
+    #print("Checking the item first ")
+    #print(check_item(user_input))
+
+    #print("Getting the price now. ")
+    #print(get_price(user_input))
+
     
-    user_input = input()
-    i = 0
+    #i = 0
     message = [
-        {"role":"system","content":"You are a order taking agent. Your job is to ask the user what they want "
-        "to order and based on that you need to place their order and show what their total bill is.  The"
-        "user might also order 2 or more items of the same kind. So take care of that as well. Use all the tools"
-        "that you have been provided access to. If the user does not mention how many quantities of the food item they want,"
-        "assume by default that they want 1 quantity of the food item. "},
+        {"role":"system","content":"You are an order taking agent. Your job is to take the users food order."
+        "You have access to certain functions that performs functions as such - taking in the user order,"
+        "checking if the ordered food item is available or not, updating the user's order, cancelling an order"
+        "if the user requests for it, or cancelling all the orders if the user requests so, printing the final bill for the user."
+        "You have access to the menu file that has already been converted from an image pdf file to a dictionary that will be"
+        "used to traverse the food items that are available."
+        "Also all the prices are to be in Indian Rupees, use it's symbol and not dollars"
+        },
         {"role":"user","content": user_input.lower()}
         ]
     
@@ -330,10 +430,14 @@ def main():
                         food_item= function_args.get("food_item")
                     )
                 
-                if function_name == "update_order":
-                    function_response = update_order(
-                        food_item= function_args.get("food_item"),
-                        quantity= function_args.get("quantity")
+                if function_name == "get_price":
+                    function_response = get_price(
+                        food_item= function_args.get("food_item")
+                    )
+                
+                if function_name == "check_item":
+                    function_response = check_item(
+                        food_item= function_args.get("food_item")
                     )
 
                 if function_name == "add_order":
@@ -344,6 +448,9 @@ def main():
                 
                 if function_name == "print_final_bill":
                     function_response = print_final_bill()
+                
+                if function_name == "print_menu":
+                    function_response = print_menu()
                 
                 if function_name == "done_ordering":
                     function_response = done_ordering()
@@ -376,6 +483,8 @@ def main():
                 "content":user_input
             }
         )
+
+        
     
 
 if __name__ == "__main__":
